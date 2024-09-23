24ef40204902a6241a0fbdc6eda755262d49ee65
denis23x <damage.23x@gmail.com>
2024-09-23 20:00:45 +0700

    feat: made post description optional field

---
+++ src/database/migrations/20240923092539_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `PostPassword` MODIFY `description` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `PostPrivate` MODIFY `description` VARCHAR(255) NULL;
--- src/schema/parts/post/parts-post-description.schema.ts
+++ src/schema/parts/post/parts-post-description.schema.ts
@@ -3,6 +3,7 @@
 export const partsPostDescriptionSchema: Record<string, any> = {
   $id: 'partsPostDescriptionSchema',
   type: 'string',
+  nullable: true,
   example: 'Dolor sit amet, co.',
   minLength: 16,
   maxLength: 192
